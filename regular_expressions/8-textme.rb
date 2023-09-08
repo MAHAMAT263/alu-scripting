1ef3e58b8144df31dfd7814779a2d86cbd0dbe37
MAHAMAT263 <m.ahmat@alustudent.com>
2023-09-08 14:53:38 -0700

    Regex is the best

--- regular_expressions/8-textme.rb
+++ regular_expressions/8-textme.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
-A = ARGV[0].scan(/(?<=from:)(.\d+)/)
-B = ARGV[0].scan(/(?<=to:)(.\d+)/)
-C = ARGV[0].scan(/(?<=falge:)([0-9|:|-]+)/)
+A = ARGV[0].scan(/(?<=SENDER:)(.\d+)/)
+B = ARGV[0].scan(/(?<=RECEIVER:)(.\d+)/)
+C = ARGV[0].scan(/(?<=FLAGS:)([0-9|:|-]+)/)
 Group = [A , B , C]
 puts Group.join
